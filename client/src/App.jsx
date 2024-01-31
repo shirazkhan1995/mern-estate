873992861bf6caecda1834237cdb08b59d1e2a5f
shirazkhan1995 <shirazkhan1995@gmail.com>
2024-01-31 12:37:21 +0530

    complete signup page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
